cc22263218ac66739bc7eeb630183a10c134f010
MicaelaFrancoTorres <micaelasofia0402@gmail.com>
2026-09-09 19:37:46 -0300

    feat: add helmet, cors and global rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
@@ -7,8 +9,25 @@ import { ViajesModule } from './viajes/viajes.module';
 import { ActividadesModule } from './actividades/actividades.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, ViajesModule, ActividadesModule],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 20,
+      },
+    ]),
+    PrismaModule,
+    AuthModule,
+    ViajesModule,
+    ActividadesModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
