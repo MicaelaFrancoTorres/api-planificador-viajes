e124c0929acdb1cb5e4fd371f965e1111d69a0d9
MicaelaFrancoTorres <micaelasofia0402@gmail.com>
2026-09-09 21:09:52 -0300

    feat: add swagger documentation and env example

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
+import cors from 'cors';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.use(cors());
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -13,6 +19,16 @@ async function bootstrap() {
     }),
   );
 
+  const config = new DocumentBuilder()
+    .setTitle('API Planificador de Viajes')
+    .setDescription('API para gestionar viajes y actividades, con autenticación JWT')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap()
